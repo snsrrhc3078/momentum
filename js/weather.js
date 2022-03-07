dc9b72e9af5df5a8ef33dbafda49d319567bc27b
snsrrhc3078 <98512002+snsrrhc3078@users.noreply.github.com>
2022-03-07 10:28:30 +0900

    make weather functional, learning CSS.

--- js/weather.js
+++ js/weather.js
@@ -4,23 +4,29 @@ const API_KEY = "4a97288af10c7b63369ecae985f8e612";
 
 
 
-function onGeoOk(position){
+function onGeoOk(position) {
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
 
     const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
-        const temp = document.querySelector("#weather span:first-child");
-        const weather = document.querySelector("#weather span:nth-child(2)");
-        const city = document.querySelector("#weather span:nth-child(3)");
 
+
+        const icon = document.querySelector("#weather-condition img");
+        const weather = document.querySelector("#weather-condition span:nth-child(2)");
+        const temp = document.querySelector("#weather-condition span:nth-child(3)");
+        const location = document.querySelector("#weather-condition span:nth-child(4)");
+
+
+        icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        weather.innerText = data.weather[0].description;
         temp.innerText = `${data.main.temp}°C`;
-        weather.innerText = data.weather[0].main;
-        city.innerText = data.name;
+        location.innerText = data.name;
+
     })
 }
 
-function onGeoError(){
+function onGeoError() {
     alert("Can't find you. no weather for you.");
 }
 
